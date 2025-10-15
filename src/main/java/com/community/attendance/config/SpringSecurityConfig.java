c1a69d601f968440d183436176967d95cf924d5e
dongminAP <tyg564@teamap.co.kr>
2025-10-15 13:01:54 +0900

    chore: spring security, valid 의존성, config 클래스 추가

---
+++ src/main/java/com/community/attendance/config/SpringSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.community.attendance.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
